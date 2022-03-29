c60c71f1629ad3c8958d5bd981fb416cfdf41992
Paulo Oliveira <paulo.hao@hotmail.com>
2022-03-28 21:28:43 -0300

    form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
-import { Dashboard } from './components/Dashboard';
-import { Header } from './components/Header';
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-import { GlobalStyle } from './styles/global';
+import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root'); // accessibility 
 
@@ -24,12 +25,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}
+      />
 
       <GlobalStyle />
     </>
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,57 +1,80 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-:root {
-    --background: #f0f2f5;
-    --red: #e52e4d;
-    --green: #33cc95;
-    --blue: #5429cc;
-    --blue-light: #6933ff;
-    --text-title: #363f5f;
-    --text-body: #969cb3;
-    --shape: #ffffff;
-}
-
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-//font-size: 16px (Desktop)
-//REM = 1rem = 16px
-html {
-    @media (max-width: 1080px) {
-        font-size: 93.75%; // 15px = 1rem
+    :root {
+        --background: #f0f2f5;
+        --red: #e52e4d;
+        --green: #33cc95;
+        --blue: #5429cc;
+        --blue-light: #6933ff;
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+        --shape: #ffffff;
     }
 
-    @media (max-width: 720px) {
-        font-size: 87.5%; // 14px = 1rem
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
-}
 
-//REM = 1rem = 16px
+    //font-size: 16px (Desktop)
+    //REM = 1rem = 16px
+    html {
+        @media (max-width: 1080px) {
+            font-size: 93.75%; // 15px = 1rem
+        }
 
-body {
-    background: var(--background);
-    -webkit-font-smoothing: antialiased;
-}
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px = 1rem
+        }
+    }
 
-body, input, textarea, button {
-    font-family: 'Poppins', sans-serif;
-    font-weight: 400;
-}
+    //REM = 1rem = 16px
 
-h1, h2, h3, h4, h5, h6, strong {
-    font-weight: 600;
-}
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
 
-button {
-    cursor: pointer; //botão clicável
-}
+    body, input, textarea, button {
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
 
-[disabled] { //tudo que estiver disabilitado
-    opacity: 0.6;
-    cursor: not-allowed;
-}
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 600;
+    }
+
+    button {
+        cursor: pointer; //botão clicável
+    }
+
+    [disabled] { //tudo que estiver disabilitado
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+
+    .react-modal-overlay {
+        background: rgba(0, 0, 0, 0.5);
+
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .react-modal-content {
+        width: 100%;
+        max-width: 576px;
+        background: var(--background);
+        padding: 3rem;
+        position: relative;
+        border-radius: 0.25rem;
+    }
 `
