6ed91e9098a026dc5dc731bb9729628ff5787c55
Paulo Oliveira <paulo.hao@hotmail.com>
2022-03-29 17:48:18 -0300

    modal_forms

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs'; // or { Server }
+import { createServer, Model } from 'miragejs'; // or { Server }
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-03-29 16:08:00'),
+        },
+        {
+          id: 2,
+          title: 'Parcela Ap',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 610,
+          createdAt: new Date('2022-03-29 16:08:00'),
+        },
+      ],
+    })
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createDate: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('/transaction', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
